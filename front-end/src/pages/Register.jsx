65acedd7a4c41726770b878f93db2d97aee73ed8
[AnasAA98] <[aaffouteanas@gmail.com]>
2024-02-17 13:44:31 -0500

    changed the the register,Login, and the Reset password page

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,8 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import Fireserver from '../Fireserver';
-
 const { userDatabase, userDataDatabase } = Fireserver;
+import Manhattan from "../Public/Photos/Manhattan.png"; 
+import "./Register.css";
 
 function Login() {
   const history = useNavigate(); // Routes
@@ -38,17 +39,20 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1> Register </h1>
-      <form onSubmit={handleSignUp}>
-        <input type="text" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <input type='text' name='firstname' placeholder="First Name" required />
-        <input type='text' name='lastinitial' placeholder="Last Initial" required />
-        <button type="submit">Signup</button>
-      </form>
+    <div className="Register">
+      <img className="Manhattan" src={Manhattan} alt="Manhattan" />
+      <div className="Register-Form"> {/* Updated class name for styling */}
+        <h1 className="Register-Text"> Register </h1>
+        <form onSubmit={handleSignUp}>
+          <input className="Email-container" type="text" name="email" placeholder="Email" required />
+          <input className="Password-container" type="password" name="password" placeholder="Password" required />
+          <input className="Fname-container" type='text' name='firstname' placeholder="First Name" required />
+          <input className="Lname-container" type='text' name='lastinitial' placeholder="Last Initial" required />
+          <button className="Signup-container" type="submit">SignUp</button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Login;
